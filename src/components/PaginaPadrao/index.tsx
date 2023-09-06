698f9d33d9e2ab6999ad224d3bab5fc6f8162c4f
Daniel Valadão <daniel.valadao@hotmail.com>
2023-09-06 18:28:23 -0300

    Finalizando o projeto

---
+++ src/components/PaginaPadrao/index.tsx
@@ -0,0 +1,19 @@
+import styles from './PaginaPadrao.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import { Outlet } from 'react-router-dom';
+
+export default function PaginaPadrao({ children }: { children?: React.ReactNode }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
+      <div className={stylesTema.container}>
+        <Outlet />
+        {children}
+      </div>
+    </>
+  );
+}
